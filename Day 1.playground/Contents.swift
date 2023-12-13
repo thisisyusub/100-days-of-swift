c9e4dd28dd526f3e4b2fdfcaae63310a3ff76b3f
thisisyusub <kanan.yusub@gmail.com>
2023-12-13 13:22:02 +0400

    day 1 added

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+// Variables
+var str = "Hello, playground"
+
+// Strings and integers
+var age = 38
+var population = 8_000_000
+
+// Multi-line strings
+var multi1 = """
+This goes
+over multiple
+lines
+"""
+
+var multi2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+var score = 85
+var interpolation = "Your score was \(score)"
+
+// Constants
+let taylor = "swift"
+
+// Type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
